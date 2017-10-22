c1721ceaee253aea96d3bf2fb74bed69cfd62059
Shreya Rangarajan <Shreya.Rangarajan@students.olin.edu>
2017-10-22 11:08:54 -0400

    Commit 1-Shreya

--- game.py
+++ game.py
@@ -12,13 +12,14 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 car = "audi_1.png"
-
+barrier = "concrete.png"
 # road = "road1.jpg"
 # road = "road2.jpg"
-road = "road3.jpg"
+road = "road.png"
 
 background_size = (1024, 750)
 car_size = (150, 100)
+concrete = (200,113)
 
 # initializing
 pygame.init()
@@ -28,6 +29,7 @@ screen = pygame.display.set_mode(background_size)
 clock = pygame.time.Clock()
 background_image = pygame.image.load(road).convert()
 player_image = pygame.image.load(car).convert()
+concrete_img = pygame.image.load(barrier).convert()
 
 background_colour = (WHITE)
 pygame.display.set_caption('Skyroads')
@@ -36,18 +38,27 @@ player_image.set_colorkey(WHITE)
 screen.blit(background_image, (0, 0))
 pygame.display.flip()
 running = True
-speed = 3
+speed = 2
 # Starting variables
 score = 0
-x_speed = 0
-y_speed = 0
+x_speed_coord = 0
+y_speed_coord = 0
+x_speed_concrete = 0
+y_speed_concrete = 0
 
-# Current position
-x_coord = 10
-y_coord = 10
+# Initial position
+x_car_coord = 420
+y_car_coord = 10
+x_concrete_coord = 420
+y_concrete_coord = 1024
+
+#Initial direction
+dx = 2
+dy = 5
 
 # Speed in pixels per frame
 while running:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -55,13 +66,13 @@ while running:
         elif event.type == pygame.KEYDOWN:
         # Figure out if it was an arrow key. If so adjust speed.
             if event.key == pygame.K_LEFT:
-                x_speed = -1 * speed
+                x_speed_coord = dx + speed
             elif event.key == pygame.K_RIGHT:
-                x_speed = speed
+                x_speed_coord = abs(dx-speed)
             elif event.key == pygame.K_UP:
-                y_speed = -1 * speed
+                y_speed_coord = dy + speed
             elif event.key == pygame.K_DOWN:
-                y_speed = speed
+                y_speed_coord = abs(dy-speed)
             # press escape to get out of game
             elif event.key == pygame.K_ESCAPE:
                 running = False
@@ -69,14 +80,17 @@ while running:
         elif event.type == pygame.KEYUP:
             # If it is an arrow key, reset vector back to zero
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                x_speed = 0
+                x_speed_coord = 0
             elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                y_speed = 0
+                y_speed_coord = 0
         # --- Game Logic
         # Move the object according to the speed vector.
-    x_coord = x_coord + x_speed
+    x_coord = x_car_coord + x_speed_coord
     # y_coord = y_coord + y_speed
     y_coord = background_size[1]*0.98 - car_size[1]
+    #player_image.x = x_coord
+    #player_image.y = y_coord
+
     screen.blit(background_image, (0, 0))
     # print(x_coord, y_coord)
     screen.blit(player_image, [x_coord, y_coord])
